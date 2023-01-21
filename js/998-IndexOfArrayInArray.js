8d758c9a95ce2b1e3f85e5ba0641ccdc38a35746
Vytautas Valutis <dnc908@gmail.com>
2023-01-22 01:36:27 +0200

    work (errors catch)

--- js/998-IndexOfArrayInArray.js
+++ js/998-IndexOfArrayInArray.js
@@ -1,11 +1,14 @@
 console.clear();
 // https://www.codewars.com/kata/5783ef69202c0ee4cb000265/train/javascript
 function IndexOfArrayInArray(arrayToSearch, query) {
-    // console.log(typeof query[0], query[0]);
-    // console.log(typeof query[1], query[1]);
-    // if (typeof query[0] === `undefined`) {return "throw error";}
-    // if (typeof query[1] === `undefined`) {return "throw error";}
-    if (!Array.isArray(query)) {return "throw error";}
+    console.log(query);
+    console.log(typeof query);
+    console.log(typeof query[0], query[0]);
+    console.log(typeof query[1], query[1]);
+    if (!Array.isArray(query)) {
+        throw `Error`;
+        // return "throw error";
+    }
     if (query.length !== 2) {return "throw error";}
     if (!(typeof query[0] === `string`) &&  !(typeof query[0] === `number` && isFinite(query[0]))) {return "throw error";}
     if (!(typeof query[1] === `string`) && !(typeof query[1] === `number` && isFinite(query[1]))) {return "throw error";}
@@ -21,7 +24,9 @@ function IndexOfArrayInArray(arrayToSearch, query) {
         if (arrayToSearch[i][0] === query[0] && arrayToSearch[i][1] === query[1]) return i;       
     }
     return -1;
+    catch()
 }
+
 // console.log(`* `,IndexOfArrayInArray([[1,2],[`a`,`b`],[7,8],[5,6]],[2,3]));
 // console.log(`0 `,IndexOfArrayInArray([[1,2],[3,4],[5,6]],[1,2]));
 // console.log(`2 `,IndexOfArrayInArray([[1,2],[3,4],[5,6]],[5,6]));
@@ -37,5 +42,5 @@ function IndexOfArrayInArray(arrayToSearch, query) {
 // console.log(`-1 `,IndexOfArrayInArray([[1,2],[3,4],[5,6]],[`AB`,`AC`]));
 // console.log(`1 `,IndexOfArrayInArray([[1,2],[`AB`,`AC`],[5,6]],[`AB`,`AC`]));
 // console.log(`-1 `,IndexOfArrayInArray([[1,2],[`AB`,`AC`],[5,6]],[`A`,`B`]));
-console.log(`* `,IndexOfArrayInArray([[1,2],[11,0],[5,6]]));
+console.log(`* `,IndexOfArrayInArray([[1,2],[11,0],[5,6]],7));
 
