6e63606cce45e3967443da5ff15732ab5fc5bb14
Vytautas Valutis <dnc908@gmail.com>
2023-01-24 14:47:17 +0200

    988 Ok

--- js/988-doors.js
+++ js/988-doors.js
@@ -1,7 +1,11 @@
 console.clear();
+// https://www.codewars.com/kata/57c15d314677bb2bd4000017/train/javascript
+
 function test(n) {
     return Math.floor(Math.sqrt(n));
 }
+
+// doors=(n)=>Math.floor(Math.sqrt(n))
 
 console.log(test(100000));
 console.log(test(5));
