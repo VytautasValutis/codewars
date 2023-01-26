548a612a51fa1b95c86940259c881765904acd3b
Vytautas Valutis <dnc908@gmail.com>
2023-01-26 12:54:06 +0200

    984  OK

--- js/984-decomp.js
+++ js/984-decomp.js
@@ -2,52 +2,43 @@ console.clear();
 // https://www.codewars.com/kata/5a045fee46d843effa000070
 function prep(n) {
     console.log(n);
-    // let psk = [[2,0],[3,0],[5,0],[7,0],[11,0],[13,0],[17,0],[19,0],[23,0],[29,0],[31,0],[37,0],[41,0],[43,0],[47,0],[53,0],[59,0],[61,0]];
-    let psk =[[]];
+    let psk ={};
     let rez = ``;
     let k = 2;
+
     function fak(n, k) {
         if(n <= 1) {return 0;}
         if( n % k === 0 ) {
-            psk[k-2][0] = 2;
-            psk[k-2][1]++;
-            n = n / psk[k-2][0];
-            fak(n,k);
+            if(k in psk) {
+                    psk[k]++;
+                } else {
+                    psk[k] = 1;
+            }
+            n /= k;
+            fak(n, k);
         } else {
             k++;
             fak(n, k);
         }
     }
-    console.log(psk);
-    return 999;
     for (let j = n; j > 1; j--) {
         fak(j, k);
-        if(prime) {return ''+n;}
     }
-    for (let i = 0; i < 18; i++) {
-        if (psk[i][1] > 0) {
-            if(i > 0) {rez = rez + ' * '}
-            rez = rez + psk[i][0];
-            if(psk[i][1] > 1) {
-                rez = rez +`^`+psk[i][1];
-            }
+    for (const el in psk) {
+        rez += ' * ' + el;
+        if(psk[el] > 1) {
+            rez += '^' + psk[el];
         }        
     }
+    rez = rez.substring(3, rez.length);
     return rez;
-
-    // function sum(start) {
-    //     if (start > 0) {
-    //       return start + sum(start - 1);
-    //     } else {
-    //       return 0;
-    //     }
-    //   }
-
 }
 
 
-console.log('5 => ',prep(5));
-console.log('23 => ',prep(23));
-console.log('79 =>',prep(79));
-// console.log('62 => ',prep(62));
+console.log('1 => ',prep(4));
+console.log('2 => ',prep(22));
+console.log('3 =>',prep(12));
+console.log('4 => ',prep(22));
+console.log('5 => ',prep(25));
+console.log('6 => ',prep(101));
 // console.log('3989 => ',prep(3989));
--- js/test.js
+++ js/test.js
@@ -1,39 +1,27 @@
 console.clear();
-function prep(n) {
-    console.log(n);
-    let psk = [[2,0],[3,0],[5,0],[7,0],[11,0],[13,0],[17,0],[19,0],[23,0],[29,0],[31,0],[37,0],[41,0],[43,0],[47,0],[53,0],[59,0],[61,0]];
-    let rez = ``;
-    let k = 0;
-    function fak(n, k) {
-        if(n <= 1) {return 0;}
-        if( n % psk[k][0] === 0 ) {
-            n = n / psk[k][0];
-            psk[k][1]++;
-            fak(n,k);
-        } else {
-            k++;
-            fak(n, k);
-        }
-    }
-    fak(n, k);
-    console.log(psk);
-    for (let i = 0; i < 18; i++) {
-        if (psk[i][1] > 0) {
-            rez = rez + psk[i][0]+`^`+psk[i][1]+` `;
-        }        
-    }
-    return rez.trimEnd();
+function test(n) {
+    const ttt = [
+        {
+            num: 0,
+            count: 0
+        },
+        {
+            num: 1,
+            count: 0
+        },
+        {
+            num: 2,
+            count: 0
+        },
+        {
+            num: 3,
+            count: 0
+        },
+    ];
 
-    // function sum(start) {
-    //     if (start > 0) {
-    //       return start + sum(start - 1);
-    //     } else {
-    //       return 0;
-    //     }
-    //   }
-
+    return ttt[num].indexOf(3);
 }
 
 
-console.log(prep(5));
+console.log(test(3));
 
