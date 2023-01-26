abc4a8dd354acc20647d9108592c8873abce13ca
Vytautas Valutis <dnc908@gmail.com>
2023-01-26 17:49:38 +0200

    Work

--- js/test.js
+++ js/test.js
@@ -1,59 +1,28 @@
 console.clear();
-function test(score) {
-    console.log(score);
-    let rez = {
-        'gold': {
-            'score': -Infinity,
-            'players': [],
-        },
-        'silver': {
-            'score': -Infinity,
-            'players': [],
-        },
-        'bronze': {
-            'score': -Infinity,
-            'players': [],
-        },
-    };
-    for(const name in score) {
-        if(score[name].length === 0) { delete score[name]; continue } 
-            const sumPoints = score[name].reduce((sum, num) => sum + num, 0);
-            if(sumPoints > rez.gold.score) {rez.gold.score = sumPoints;} else
-            if(sumPoints > rez.silver.score) {rez.silver.score = sumPoints;} else
-            if(sumPoints > rez.bronze.score) {rez.bronze.score = sumPoints;}
-            score[name]['sum'] = sumPoints;
+function test(rec, deze) {
+    console.log(rec, deze);
+    let kiekis = [];
+    let k = 0;
+    for(const produktas in rec) {
+        for(const maiselis in deze) {
+            if(produktas === maiselis) {
+                console.log(produktas, maiselis);
+                console.log(rec[produktas],deze[maiselis]);                
+                if(rec[produktas] > deze[maiselis]) {return 0;};
+                kiekis[k++] = Math.floor(deze[maiselis] / rec[produktas]);
+                continue;
+            }
+        }
     }
-    for(const name in score) {
-        if(score[name]['sum'] === rez.gold.score ) {rez.gold.players.push(name);} else
-        if(score[name]['sum'] === rez.silver.score ) {rez.silver.players.push(name);} else
-        if(score[name]['sum'] === rez.bronze.score ) {rez.bronze.players.push(name);}; 
-    }
-    rez.gold.players.sort()
-    rez.silver.players.sort()
-    rez.bronze.players.sort()
-    if(rez.silver.players.length === 0) {
-        delete rez.silver;
-    }
-    if(rez.bronze.players.length === 0) {
-        delete rez.bronze;
-    }
-    return rez;;
+    console.log(rec, kiekis);
+
+
+    return '***';
 }
 
 
-const test1 = {
-    'Emy': [8, 12, 1],
-    'Paul': [2, 6, -9],
-    'Sandra': [3, 9, 7],
-    'Bjaka': [],
-    'Sue': [10, 2, 0],
-    'Kukis': [8, 2, 2],
-    'Yeseult': [8, 9, 3],
-}
-const test2 = {
-    'Emy': [8, 12, 1],
-    'Paul': [2, 6, -9],
-    'Bjaka': [],
-}
-console.log(test(test1));
 
+
+
+console.log(test({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
+// console.log(test({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
