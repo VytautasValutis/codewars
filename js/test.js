b1afafa547bc7ae350e0e37f77543e563501f82f
Vytautas Valutis <dnc908@gmail.com>
2023-01-29 17:37:40 +0200

    Work

--- js/test.js
+++ js/test.js
@@ -1,27 +1,72 @@
 console.clear();
-function testas(s1, s2) {
-    let rez = [];
-    let i = 0;
-    let j = 0;
-    while(i < s1.length && j < s2.length) {
-        if(s1[i] <= s2[j]) {
-            rez.push(s1[i++]);
-        } else {
-            rez.push(s2[j++]);
+function testas(arr) {
+    let rez = '' + arr[0];
+    let diff = 0;
+    let cond = {
+            type: -2,
+            first: -1,
+            count: 0,
+        };
+    for (let i = 1; i < arr.length; i++) {
+        diff = a[i] - a[i - 1];
+        switch (cond.type) {
+            case -2:
+                change(a[i - 1], diff, true);                
+                break;
+            case 0:
+                if(diff === 0) {
+                    cond.count++;
+                    continue;
+                } else {
+                    sequenc('*', cond.count);
+                    continue;
+                }
+                break;
+            case 1:
+                if(diff === 1) {
+                    cond.count++;
+                    continue;
+                } else  if(cond.count > 2){
+                    sequenc('-', cond);
+                    continue;
+                } else {
+
+                }
+                break;
+            case -1:
+                
+                break;
+            default: // x
+                break;
         }
-    }
-    while( i < s1.length) {
-        rez.push(s1[i++]);
-    }
-    while( j < s2.length) {
-        rez.push(s2[j++]);
     }
     return rez ;
 }
+function change(item, difChange, plus) {
+    cond.type = difChange;
+    cond.first = item;
+    if(plus) {
+        cond.count++;
+    } else {
+        cond.count = 1;
+    }
+    return 0;
+}
+function sequenc(par, countSeq) {
+    switch(par) {
+        case '*': 
+            rez += cond.first + par + countSeq;
+            cond.count = 0;
+            cond.type = -2;
+            cond.first = -1
+            break;
+        
+    }
+    
+}
 
-console.log(testas([1, 2, 3, 4, 5],[1, 2, 4, 7, 8]));
-console.log(testas([10],[1, 2, 4, 7, 8]));
-console.log(testas([],[1, 2, 4, 7, 8]));
-console.log(testas([],[]));
-console.log(testas([1, 2, 3, 4, 5, 66],[1, 2, 4, 7, 8]));
+console.log(testas([1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
+// console.log(testas([0, 2, 4, 5, 7, 8, 9]));
+// console.log(testas([0, 2, 4, 5, 7, 6, 5]));
+// console.log(testas([0, 2, 4, 5, 7, 6, 5, 5, 5, 5, 5]));
 
