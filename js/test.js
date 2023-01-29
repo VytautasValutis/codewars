4dcb9e54f86140b4657ab79d1981f8bfc67a2041
Vytautas Valutis <dnc908@gmail.com>
2023-01-29 23:06:07 +0200

    Work

--- js/test.js
+++ js/test.js
@@ -1,6 +1,38 @@
 console.clear();
 function testas(arr) {
-    let rez = '' + arr[0];
+    // console.log(arr);
+    console.log('=======================');
+    function change(difChange, first, plus) {
+        if(plus) {
+            cond.count++;
+        } else {
+            cond.count = 2;
+            rez += cond.first + ',';
+        }
+        cond.type = difChange;
+        cond.first = first;
+        return 0;
+    }
+    function sequenc(par, countSeq) {
+        switch(par) {
+            case '*': 
+                rez += cond.first + par + countSeq +',';
+                break;
+            case '-': 
+                rez += cond.first + par + countSeq + ',';
+                break;
+            case '/': 
+                rez += cond.first + par + countSeq +',';
+                break;
+            default:
+                break;            
+            }
+        cond.count = 0;
+        cond.type = -2;
+        cond.first = -1;
+        return 0;
+    }
+    let rez = '';
     let diff = 0;
     let cond = {
             type: -2,
@@ -8,12 +40,15 @@ function testas(arr) {
             count: 0,
         };
     for (let i = 1; i < arr.length; i++) {
-        diff = a[i] - a[i - 1];
+        diff = arr[i] - arr[i - 1];
+        console.log(' i => ', i, diff, cond, arr[i]);
         switch (cond.type) {
             case -2:
-                change(a[i - 1], diff, true);                
+                console.log('-2 => ', i, diff, cond, arr[i]);
+                change(diff, arr[i - 1], true);                
                 break;
             case 0:
+                console.log(' 0 => ', i, diff, cond, arr[i]);
                 if(diff === 0) {
                     cond.count++;
                     continue;
@@ -23,49 +58,52 @@ function testas(arr) {
                 }
                 break;
             case 1:
+                console.log(' 1 => ', i, diff, cond, arr[i]);
                 if(diff === 1) {
                     cond.count++;
                     continue;
                 } else  if(cond.count > 2){
-                    sequenc('-', cond);
+                    sequenc('-', arr[i - 1]);
+                    console.log(' 1 +> ', rez);
                     continue;
                 } else {
-
+                    change(diff, arr[i - 1], false);
+                    continue;
                 }
                 break;
             case -1:
-                
+                console.log('-1 => ', i, diff, cond, arr[i]);
+                if(diff === -1) {
+                    cond.count++;
+                    continue;
+                } else  if(cond.count > 2){
+                    sequenc('-', arr[i - 1]);
+                    continue;
+                } else {
+                    change(diff, arr[i - 1], false);
+                    continue;
+                }
                 break;
             default: // x
+            console.log(' x => ', i, diff, cond, arr[i]);
+            if(diff > 1 ) {
+                    cond.count++;
+                    continue;
+                } else  if(cond.count > 2){
+                    sequenc('/', cond.count);
+                    continue;
+                } else {
+                    change(diff, arr[i - 1], false);
+                    console.log(' x +> ', rez);
+                    continue;
+                }
                 break;
+                }
+            }
+            return rez ;
         }
-    }
-    return rez ;
-}
-function change(item, difChange, plus) {
-    cond.type = difChange;
-    cond.first = item;
-    if(plus) {
-        cond.count++;
-    } else {
-        cond.count = 1;
-    }
-    return 0;
-}
-function sequenc(par, countSeq) {
-    switch(par) {
-        case '*': 
-            rez += cond.first + par + countSeq;
-            cond.count = 0;
-            cond.type = -2;
-            cond.first = -1
-            break;
-        
-    }
-    
-}
 
-console.log(testas([1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
+console.log('==> 1,3-5,7-11,14*2,17-20 ==> ',testas([1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 14, 17, 18, 19, 20]));
 // console.log(testas([0, 2, 4, 5, 7, 8, 9]));
 // console.log(testas([0, 2, 4, 5, 7, 6, 5]));
 // console.log(testas([0, 2, 4, 5, 7, 6, 5, 5, 5, 5, 5]));
