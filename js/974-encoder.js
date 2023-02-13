81a3f54a44c4289a7852c46b2d5c2637d674b6be
Vytautas Valutis <dnc908@gmail.com>
2023-02-13 17:20:15 +0200

    973 tremp

--- js/974-encoder.js
+++ js/974-encoder.js
@@ -2,96 +2,70 @@ console.clear();
 // https://www.codewars.com/kata/58db9545facc51e3db00000a
 function testas(arr) {
     console.log(arr);
-    function change(difChange, first, plus) {
-        if(plus) { cond.count++; } else {
-            cond.count = 2;
-            rez += cond.first + ',';
-        }
-        cond.type = difChange;
-        cond.first = first;
-        return 0;
-    }
-    function sequenc(par, countSeq, last) {
-        switch(par) {
-            case '*': 
-                rez += cond.first + par + countSeq +',';
-                break;
-            case '-': 
-                rez += cond.first + par + countSeq + ',';
-                break;
-            case '/': 
-                rez += cond.first + '-' + last + par + countSeq +',';
-                break;
-            default:
-                break;            
-            }
-        cond.count = 0;
-        cond.type = -2;
-        cond.first = -1;
-        return 0;
-    }
-    arr.push(Number.MAX_SAFE_INTEGER);
     let rez = '';
-    let diff = 0;
+    let temp= '';
     let cond = {
-            type: -2,
-            first: -1,
-            count: 1,
+            c: 2,
+            d1: 0,
+            d2: 0,
         };
-    for (let i = 1; i <= arr.length; i++) {
-        diff = arr[i] - arr[i - 1];
-        switch (cond.type) {
-            case -2:
-                change(diff, arr[i - 1], true);                
-                break;
-            case 0:
-                if(diff === 0) {
-                    cond.count++;
-                    continue;
-                } else {
-                    sequenc('*', cond.count);
-                    continue;
+    const n = arr.length;
+    if(n <= 2) {
+        if(n === 2) {
+            temp = '' + arr[0] + ',' + arr [1];
+            rez += temp;
+        }
+        if(n === 1) {
+            temp = '' + a[0];
+            rez += temp;
+         } 
+        return rez;
+    };
+    rez = arr[0];
+    for(let i = 1; i <= n - 1; i++) {
+        cond.d1 = arr[i - 1] - arr[i];
+        cond.d1 = arr[i] - arr[i + 1];
+        if(i === n - 2){
+            if(cond.d1 === 0) {
+                cond.c++;
+                if(cond.d1 === cond.d2) {
+                    cond.c++;
+                    temp = '*' + cond.c; 
+                } else {temp = '*' + cond.c + ',' + arr[i + 1];}
+            } else if(cond.d1 === cond.d2) {
+                cond.c++;
+                if(cond.c > 2) {
+                    if(Math.abs(cond.d2) > 1) {
+                        temp = '-' + arr[i + 1] + '/' + Math.abs(cond.d2);
+                    } else {temp = '-' + arr[i + 1];}
+                } else {temp = ',' + arr[i] + ',' + arr[i + 1];}
+            } else {temp = ',' + arr[i] + ',' + arr[i + 1];}
+            rez += temp;
+            return rez;
+        }
+        if(cond.d1 === cond.d2) {cond.c++}
+        else {
+            if(cond.d1 === 0) {
+                if(cond.c <= 2) {temp = ',' + arr[i];
+                } else if( Math.abs(cond.d1) > 1) {
+                    temp = '-' + a[i] + '/' + Math.abs(d1);
                 }
-                break;
-            case 1:
-                if(diff === 1) { cond.count++; continue;
-                } else  if(cond.count > 2){
-                    sequenc('-', arr[i - 1]);
-                    continue;
-                } else {
-                    change(diff, arr[i - 1], false);
-                    continue;
-                }
-                break;
-            case -1:
-                if(diff === -1) { cond.count++; continue;
-                } else  if(cond.count > 2){
-                    sequenc('-', arr[i - 1]);
-                    continue;
-                } else {
-                    change(diff, arr[i - 1], false);
-                    continue;
-                }
-                break;
-            default: // x
-            if(diff > 1 ) { cond.count++; continue;
-                } else  if(cond.count > 2){
-                    sequenc('/', cond.type, arr[i - 1]);
-                    continue;
-                } else {
-                    change(diff, arr[i - 1], false);
-                    continue;
-                }
-                break;
+            } else {
+                if(cond.c > 1) {
+                    temp = '*' + cond.c + ',' + arr[i + 1];
+                } else {temp = ',' + arr[i];}
+            }
+            rez += temp;
+            cond.c = 1;
         }
     }
     return rez.substring(0, rez.length - 1) ;
 }
 
 // console.log('==> 1,3-5,7-11,14*2,17-20,25 ==> ',testas([1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 14, 17, 18, 19, 20, 25]));
-// console.log(testas([0, 2, 4, 5, 7, 8, 9]));
+console.log(testas([0, 2, 4, 5, 7, 8, 9]));
 // console.log(testas([0, 2, 4, 5, 7, 6, 5]));
-// console.log(testas([0, 2, 4, 5, 7, 6, 5]));
-console.log(testas([0, 2, 4, 5, 7, 6, 5, 5, 5, 5, 5]));
-console.log(testas([2, 2, 2, 5, 5, 6, 5, 4, 3, 3]));
+// console.log(testas([0, 2, 5, 5, 7, 6, 5]));
+// console.log(testas([0, 2, 4, 5, 7, 6, 5, 5, 5, 5, 5]));
+// console.log(testas([ 1, 10, 8, 6, 7 ]));
 
--- js/test.js
+++ js/test.js
@@ -1,111 +1,14 @@
 console.clear();
-function testas(arr) {
+function testas(str) {
     console.log('-----------------------');
-    console.log(arr, arr.length);
+    console.log(str);
     console.log('=======================');
-    // arr.push(Number.MAX_SAFE_INTEGER);
-    let rez = '';
-    let diff = 0;
-    let cond = {
-            type: -2,
-            first: arr[0],
-            count: 1,
-            start: true,
-        };
-    for (let i = 1; i < arr.length; i++) {
-        diff = arr[i] - arr[i - 1];
-        //
-        console.log(' i => ', i, diff, cond, arr[i]);
-        //
-        if(cond.start) {
-            cond.type = diff;
-            cond.count++;
-            cond.start = false;
-        } else {
-            if(cond.type === 0) {
-                if(diff === 0) {
-                    cond.count++;
-                } else {
-                    rez += cond.first + '*' + (cond.count + 1) + ',';
-                    cond.count = 0;
-                    cond.first = arr[i];
-                    cond.start = true;
-                }
-            } else {
-                if(diff === cond.type){
-                    cond.count++;
-                } else {
-        //
-        console.log('01 => ', i, diff, cond, arr[i]);
-        //
-                    if(cond.count > 2) {
-                        if(cond.type === 1 || cond.type === -1) {
-                            rez += cond.first + '-' + arr[i - 1] + ',';
-                        } else {
-                            rez += cond.first + '-' + arr[i -1] + '/' + Math.abs(cond.type) + ',';
-                        }
-                        cond.count = 0;
-                        cond.start = true;
-                        cond.first = arr[i];
-                    } else {
-                        rez += cond.first + ',';
-                        cond.first = arr[i - 1];
-                        cond.count = 2;
-                        cond.type = diff;
-                    }
-        //
-        console.log('02 => ', i, diff, cond, arr[i]);
-        //
-                }
-            }
-        }
-        //
-        console.log('03 => ', i, diff, cond, arr[i]);
-        //
-        if(i === arr.length - 1) {
-            if(cond.start) {
-        //
-        console.log('04 => ', i, diff, cond, arr[i]);
-        console.log(rez);
-        //
-        rez += '' + cond.first;
-        //
-        console.log('05 => ', i, diff, cond, arr[i]);
-        console.log(rez);
-        //
-                continue;
-            }
-            switch(cond.type) {
-                case 0:
-                    if(cond.count > 0) { rez += cond.first + "*" + (cond.count + 1);
-                    } else { rez += cond.first; }
-                    break;
-                case 1:
-                case -1:    
-                    if(cond.count > 2) {
-                        rez += cond.first + '-' + arr[i];
-                    } else {
-                        rez += cond.first + arr[i];
-                    }
-                    break;
-                default:
-                    if(cond.count > 2) {
-                        rez += cond.first + '-' + arr[i] + '/' + Math.abs(cond.type);
-                    } else {
-                        rez += cond.first + arr[i];
-                    }
-                    break;
-            }
-        }
-    }
-    // console.log('** +> ', cond.count, cond.type, cond.first);
+    rez = str;
     return rez;
 }
 
-// console.log('==> 1,3-5,7-11,14*2,17-20,25 ==> ',testas([1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 14, 17, 18, 19, 20, 25]));
-// console.log(testas([0, 2, 4, 5, 7, 8, 9]));
-// console.log(testas([0, 2, 4, 5, 7, 6, 5]));
-// console.log(testas([0, 2, 4, 5, 7, 6, 5]));
-// console.log(testas([0, 2, 4, 5, 7, 6, 5, 5, 5, 5, 5]));
-console.log(testas([ 1, 10, 8, 6, 7, 8, 9 ]));
+// console.log(testas(""));
+console.log(testas("56 33 100 22 56 89 98 13 120 99"));
+// console.log(testas(" 56 33 100   22 56 89 98 13 120 99"));
+// console.log(testas("56 33 100 22 56 89 98  13 120 99  "));
 
