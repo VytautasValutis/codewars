afeed57c29d5074160f0b7052af0eb91a1cbdaef
Vytautas Valutis <dnc908@gmail.com>
2023-02-14 14:05:21 +0200

    work

--- js/test.js
+++ js/test.js
@@ -3,10 +3,36 @@ function testas(program) {
     console.log('-----------------------');
     console.log(program);
     console.log('=======================');
-    let str = {};
+    let str = {
+        name: '',
+        value: 0,
+    };
     let comNr = 0;
+    let comm = [];
     while (comNr < program.length) {
-        console.log(comNr, program[comNr]);
+        comm = program[comNr].split(" ");
+        switch (comm[0]) {
+            case 'mov':
+                if(str[comm[1]] === '') {
+                    console.log("Nera");
+                } else {
+                    console.log("Yra");
+                }
+                break;
+            case 'inc':
+                
+                break;
+            case 'dec':
+                
+                break;
+            case 'jnz':
+                
+                break;
+            default:
+                return false;
+                break;
+        }
+        console.log(comNr, program[comNr],comm);
         comNr++;
     }
     return str;
--- js/tst.js
+++ js/tst.js
@@ -1,8 +1,26 @@
 console.clear();
-function aM(a, b) {
-    console.log(a ** 2); 
+function TST(a) {
+    register = [];
+    let name = 'a';
+    let value = 1;
+    register.push({name, value});
+    name = 'b';
+    value = 2;
+    register.push({name, value});
+    name = 'c';
+    value = 3;
+    register.push({name, value});
+    console.log(register.findIndex((a) => a.name === 'c'));
+    // console.log(register.find((a) => a.name === 'd').value);
+    register.find((a) => a.name === 'b').value = 5;
+    console.log(register);
+    register.forEach(reg => {
+        console.log(reg.name);
+    });
+
+    console.log('***');
     return '***' ;
 }
 
-console.log(aM([4,5,6],[1,2,3]));
 
+console.log(TST(0));
