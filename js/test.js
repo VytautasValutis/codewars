ba09c7e8331b646e49a81da6c47a164fcf430977
Vytautas Valutis <dnc908@gmail.com>
2023-02-16 17:06:04 +0200

    Start 970

--- js/test.js
+++ js/test.js
@@ -1,45 +1,25 @@
 console.clear();
-function testas(program) {
+function testas(str) {
     console.log('-----------------------');
-    console.log(program);
+    console.log(str);
     console.log('=======================');
-    let register = {};
-    let comNr = 0;
-    let comm = [];
-    while (comNr < program.length) {
-        comm = program[comNr].split(" ");
-        switch (comm[0]) {
-            case 'mov':
-                if(Number.isInteger(+comm[2])) {
-                    register[comm[1]] = +comm[2];
-                } else {
-                    register[comm[1]] = register[comm[2]];
-                }
-                comNr++;
-                break;
-            case 'inc':
-                register[comm[1]]++;
-                comNr++;
-                break;
-            case 'dec':
-                register[comm[1]]--;
-                comNr++;
-                break;
-            case 'jnz':
-                if(register[comm[1]] !== 0) {
-                    comNr += +comm[2];
-                } else {
-                    comNr++;
-                }
-                break;
-            default:
-                return false;
+    const isLetter = (smb) => (smb.charCodeAt() >= 97 && smb.charCodeAt() <= 122) ? true : false;
+    let words = [];
+    const wordsExcl = ["a", "the", "on", "at", "of", "upon", "in", "as"];
+    let word = [];
+    let wordString = '';
+    str = str.toLowerCase();
+    for (let i = 0; i < str.length; i++) {
+        if(isLetter(str.substring(i,i+1))) {
+            word.push(str.substring(i,i+1));
+        } else {
+            wordString = word.join('');
+            word = [];
+            console.log(wordString);
         }
     }
-    return register;
+    return "***";
 }
 
-// console.log(testas(""));
-// console.log(testas(['mov a 5','inc a','dec a','dec a','jnz a -1', 'inc a']));
-console.log(testas(['mov a -10','mov b a','inc a','dec b','jnz a -2']));
+console.log(testas("Hello there, little user5453 374 ())$."));
 
