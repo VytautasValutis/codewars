789a8d3593167e851861d3b5562f047d3a9c6df3
Vytautas Valutis <dnc908@gmail.com>
2023-02-20 13:34:50 +0200

    967 test started

--- js/test.js
+++ js/test.js
@@ -3,36 +3,41 @@ function testas(n) {
     console.log('-----------------------');
     console.log(n);
     console.log('=======================');
-    let rez = '';
-    const first19 = [0,1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99];
-    const nString = n.toString().split('');
-    const nLong = nString.length;
-    const lim1 = (2 * Math.pow(10,nLong-1));
-    const lim2 = Math.floor(lim1 * 0.55);
-    if(n < 20) return BigInt(first19[n-1]);
-    if(n < 100) {
-        rez += '' + (nString[0] - 1) + nString[1] + (nString[0] - 1);
-        return BigInt(rez);
+    let rez = 1;
+    let arr = [n - 1, 1];
+    const suma = a => a.reduce((b, a) => b += a, 0); 
+    const fillArr = (a, k) => {
+        // console.log('... =>',a,k,arr );
+        arr = [];
+        arr[0] = a;
+        // console.log('... ->',a,k,arr );
+        while(suma(arr) < k) {
+            arr.push(a);
+        }
+        // console.log('... +>',a,k,arr );
+        return 0;
+    };
+    function Count(k, n) {
+        if(arr[0] === 0) return;
+        console.log(arr);
+        if(suma(arr) === n) {
+            rez++;
+            if(arr[k] > 1) {
+                arr[k]--;
+                arr.splice(k + 1, 1); 
+            } else{
+                // console.log('*** +>', k);
+                fillArr(arr[k - 1] - 1, n);
+                // console.log('*** =>',arr);
+            }
+        } else {
+            arr[k]--;
+        }
+        console.log(arr);
     }
-    if(n < lim2) {
-        rez += '9';
-        for (let i = 2; i <= nLong - 1; i++) {rez += nString[i]}
-        for (let i = 2; i <= nLong - 2; i++) {rez += nString[nLong - i]}
-        rez += '9';
-        return BigInt(rez);
-    }
-    if(n < lim1) {
-        for (let i = 1; i <= nLong - 1; i++) {rez += nString[i]}
-        for (let i = 1; i <= nLong - 1; i++) {rez += nString[nLong - i]}
-        return BigInt(rez);
-    }
-    rez += (nString[0] - 1) ;
-    for (let i = 1; i <= nLong - 1; i++) {rez += nString[i]}
-    for (let i = 2; i <= nLong - 1; i++) {rez += nString[nLong - i]}
-    rez += (nString[0] - 1) ;
-    return BigInt(rez);
+    Count(1, n)
+    return rez;
 }
 
-console.log(testas(75));
-console.log(testas(139));
+console.log(testas(6));
 
